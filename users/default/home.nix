b8207493df3016204a98435e0a15db4f8eb6b210
“lugott” <lutz0go@gmail.com>
2022-01-30 15:53:36 +0100

    changed Version to 20.09

--- users/default/home.nix
+++ users/default/home.nix
@@ -12,7 +12,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "22.05";
+  home.stateVersion = "20.09";
 
   imports = [
     # ./alacritty.nix
