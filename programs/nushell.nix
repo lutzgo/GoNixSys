1c2a17f95c4f55976139f4eff427afd603ff889c
“lugott” <lutz0go@gmail.com>
2022-01-30 17:19:36 +0100

    changed nushell.nix

--- programs/nushell.nix
+++ programs/nushell.nix
@@ -5,7 +5,7 @@
   programs.nushell = { 
     enable = true;
     settings = {
-      startup = [ "mkdir ~/.cache/starship" , "starship init nu | save ~/.cache/starship/init.nu", "source ~/.cache/starship/init.nu" ];
+      startup = [ "mkdir ~/.cache/starship" "starship init nu | save ~/.cache/starship/init.nu" "source ~/.cache/starship/init.nu" ];
       prompt = "starship_prompt";
     };
   };
