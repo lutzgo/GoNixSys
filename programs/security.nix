9f42f6b465856aef9b1682b8ac2115f4a0eba58f
“lugott” <lutz0go@gmail.com>
2022-01-30 10:29:21 +0100

    disabled security for test

--- programs/security.nix
+++ programs/security.nix
@@ -4,17 +4,17 @@
   security = {
     # rtkit is optional but recommended
     rtkit.enable = true;
-    apparmor = {
-      enable = true;
-      packages = with pkgs; [
-        apparmor-profiles
-      ];
-    };
-    pam.services.login.enableGnomeKeyring = true;
+    # apparmor = {
+    #   enable = true;
+    #   packages = with pkgs; [
+    #     apparmor-profiles
+    #   ];
+    # };
+    # pam.services.login.enableGnomeKeyring = true;
   };
 
   services = {
-    dbus.apparmor = "enabled";
+    # dbus.apparmor = "enabled";
     openssh = {
       forwardX11 = true;
       passwordAuthentication = false;
