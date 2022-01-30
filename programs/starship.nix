d1c30e1ace96303c176a3476069be8992b881da5
“lugott” <lutz0go@gmail.com>
2022-01-30 16:38:26 +0100

    changed starship.nix

--- programs/starship.nix
+++ programs/starship.nix
@@ -6,12 +6,12 @@
     enable = true;
     settings = {
       add_newline = true;
-      format = lib.concatStrings [
-        "$line_break"
-        "$package"
-        "$line_break"
-        "$character"
-      ];
+      # format = lib.concatStrings [
+      #   "$line_break"
+      #   "$package"
+      #   "$line_break"
+      #   "$character"
+      # ];
       scan_timeout = 10;
       character = {
         success_symbol = "➜";
