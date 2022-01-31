3e65aa750feaeed945218290b8d6e3a40f889db1
“lugott” <lutz0go@gmail.com>
2022-01-31 16:56:28 +0100

    corrected .starfish/default.nix

--- users/default/starship/default.nix
+++ users/default/starship/default.nix
@@ -7,7 +7,7 @@
   programs.starship = { 
     enable = true;
     settings = {
-      add_newline = true
+      add_newline = true;
     };
   };
   home.file.".config/starship.toml".source = ./starship.toml;
