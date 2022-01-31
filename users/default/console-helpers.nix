e524d64cf149c0b93f5123420f9e3c3c3035e605
“lugott” <lutz0go@gmail.com>
2022-01-31 18:08:15 +0100

    added aliases for various tools

--- users/default/console-helpers.nix
+++ users/default/console-helpers.nix
@@ -9,20 +9,31 @@
     bat-extras.batwatch
     bat-extras.prettybat
     delta
+    dust
+    exa
     fd
+    procs
     ripgrep-all
+    sd
+    tealdeer
   ];
 
-  programs.bat = {
-    enable = true;
-    # themes = {
-    #   dracula = builtins.readFile (pkgs.fetchFromGitHub {
-    #         owner = "dracula";
-    #         repo = "sublime"; # Bat uses sublime syntax for its themes
-    #         rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
-    #         sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
-    #   } + "/Dracula.tmTheme");
-    # };
+  programs = {
+    bat = {
+      enable = true;
+      # themes = {
+      #   dracula = builtins.readFile (pkgs.fetchFromGitHub {
+      #         owner = "dracula";
+      #         repo = "sublime"; # Bat uses sublime syntax for its themes
+      #         rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+      #         sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+      #   } + "/Dracula.tmTheme");
+      # };
+    };
+    exa = {
+      enable = true;
+      enableAliases = true;
+    };
   };
 
 }
--- users/default/fish.nix
+++ users/default/fish.nix
@@ -12,11 +12,16 @@
       interactiveShellInit = "starship init fish | source";
       shellAliases = {
         "cat" = "bat";
+        "catp" = "bat --plain --color";
         "codecat" = "prettybat";
         "diff" = "batdiff --delta";
+        "du" = "dust";
         "find" = "fd … -X bat";
         "grep" = "batgrep";
         "man" = "batman";
+        "ps" = "procs";
+        "sed" = "sd";
+        "tldr" = "tealdeer";
         "watch" = "batwatch";
       };
     };
