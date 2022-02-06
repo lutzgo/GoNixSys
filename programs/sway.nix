70e78f3ec16f13bce2077c05073069b8c911e0e3
“lugott” <lutz0go@gmail.com>
2022-02-06 11:17:26 +0100

    disabled sway user services

--- programs/sway.nix
+++ programs/sway.nix
@@ -2,24 +2,24 @@
 
 {
 
-  # Here we but a shell script into path, which lets us start sway.service (after importing the environment of the login shell).
-  environment.systemPackages = with pkgs; [
-    (
-      pkgs.writeTextFile {
-        name = "startsway";
-        destination = "/bin/startsway";
-        executable = true;
-        text = ''
-          #! ${pkgs.bash}/bin/bash
+  # # Here we but a shell script into path, which lets us start sway.service (after importing the environment of the login shell).
+  # environment.systemPackages = with pkgs; [
+  #   (
+  #     pkgs.writeTextFile {
+  #       name = "startsway";
+  #       destination = "/bin/startsway";
+  #       executable = true;
+  #       text = ''
+  #         #! ${pkgs.bash}/bin/bash
 
-          # first import environment variables from the login manager
-          systemctl --user import-environment
-          # then start the service
-          exec systemctl --user start sway.service
-        '';
-      }
-    )
-  ];
+  #         # first import environment variables from the login manager
+  #         systemctl --user import-environment
+  #         # then start the service
+  #         exec systemctl --user start sway.service
+  #       '';
+  #     }
+  #   )
+  # ];
 
   nixpkgs.overlays = [
     (self: super: {
@@ -67,11 +67,18 @@
         wofi-emoji
       ];
     };
-
-    # waybar.enable = true;
   };
 
   services = {
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+        };
+      };
+    }
+    };
     # redshift = {
     #   enable = true;
     #   # Redshift with wayland support isn't present in nixos-19.09 atm. You have to cherry-pick the commit from https://github.com/NixOS/nixpkgs/pull/68285 to do that.
@@ -94,83 +101,83 @@
     # waybar.enable.true;
   };
 
-  systemd.user = {
-    targets.sway-session = {
-      description = "Sway compositor session";
-      documentation = [ "man:systemd.special(7)" ];
-      bindsTo = [ "graphical-session.target" ];
-      wants = [ "graphical-session-pre.target" ];
-      after = [ "graphical-session-pre.target" ];
-    };
+  # systemd.user = {
+  #   targets.sway-session = {
+  #     description = "Sway compositor session";
+  #     documentation = [ "man:systemd.special(7)" ];
+  #     bindsTo = [ "graphical-session.target" ];
+  #     wants = [ "graphical-session-pre.target" ];
+  #     after = [ "graphical-session-pre.target" ];
+  #   };
 
-    services.kanshi = {
-      enable = true;
-      description = "Kanshi output autoconfig ";
-      wantedBy = [ "graphical-session.target" ];
-      # bindsTo = [ "sway-session.target" ];
-      partOf = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        # kanshi doesn't have an option to specifiy config file yet, so it looks
-        # at .config/kanshi/config
-        ExecStart = ''
-          ${pkgs.kanshi}/bin/kanshi
-        '';
-        RestartSec = 5;
-        Restart = "always";
-      };
-    };
+  #   services.kanshi = {
+  #     enable = true;
+  #     description = "Kanshi output autoconfig ";
+  #     wantedBy = [ "graphical-session.target" ];
+  #     # bindsTo = [ "sway-session.target" ];
+  #     partOf = [ "graphical-session.target" ];
+  #     serviceConfig = {
+  #       Type = "simple";
+  #       # kanshi doesn't have an option to specifiy config file yet, so it looks
+  #       # at .config/kanshi/config
+  #       ExecStart = ''
+  #         ${pkgs.kanshi}/bin/kanshi
+  #       '';
+  #       RestartSec = 5;
+  #       Restart = "always";
+  #     };
+  #   };
 
-    services.mako = {
-      enable = true;
-      description = "Mako notification daemon";
-      wantedBy = [ "graphical-session.target" ];
-      partOf = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "dbus";
-        BusName = "org.freedesktop.Notifications";
-        ExecStart = ''
-          ${pkgs.mako}/bin/mako
-        '';
-        RestartSec = 5;
-        Restart = "always";
-      };
-    };
+  #   services.mako = {
+  #     enable = true;
+  #     description = "Mako notification daemon";
+  #     wantedBy = [ "graphical-session.target" ];
+  #     partOf = [ "graphical-session.target" ];
+  #     serviceConfig = {
+  #       Type = "dbus";
+  #       BusName = "org.freedesktop.Notifications";
+  #       ExecStart = ''
+  #         ${pkgs.mako}/bin/mako
+  #       '';
+  #       RestartSec = 5;
+  #       Restart = "always";
+  #     };
+  #   };
 
-    services.sway = {
-      description = "Sway - Wayland window manager";
-      documentation = [ "man:sway(5)" ];
-      bindsTo = [ "graphical-session.target" ];
-      wants = [ "graphical-session-pre.target" ];
-      after = [ "graphical-session-pre.target" ];
-      # We explicitly unset PATH here, as we want it to be set by
-      # systemctl --user import-environment in startsway
-      environment.PATH = lib.mkForce null;
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = ''
-          ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/startsway --debug
-        '';
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
+  #   services.sway = {
+  #     description = "Sway - Wayland window manager";
+  #     documentation = [ "man:sway(5)" ];
+  #     bindsTo = [ "graphical-session.target" ];
+  #     wants = [ "graphical-session-pre.target" ];
+  #     after = [ "graphical-session-pre.target" ];
+  #     # We explicitly unset PATH here, as we want it to be set by
+  #     # systemctl --user import-environment in startsway
+  #     environment.PATH = lib.mkForce null;
+  #     serviceConfig = {
+  #       Type = "simple";
+  #       ExecStart = ''
+  #         ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/startsway --debug
+  #       '';
+  #       Restart = "on-failure";
+  #       RestartSec = 1;
+  #       TimeoutStopSec = 10;
+  #     };
+  #   };
 
-    services.waybar = {
-      enable = true;
-      description = "Wayland bar for Sway and Wlroots based compositors";
-      wantedBy = [ "graphical-session.target" ];
-      partOf = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = ''
-          ${pkgs.waybar}/bin/waybar
-          '';
-        RestartSec = 5;
-        Restart = "always";
-      };
-    };
+  #   services.waybar = {
+  #     enable = true;
+  #     description = "Wayland bar for Sway and Wlroots based compositors";
+  #     wantedBy = [ "graphical-session.target" ];
+  #     partOf = [ "graphical-session.target" ];
+  #     serviceConfig = {
+  #       Type = "simple";
+  #       ExecStart = ''
+  #         ${pkgs.waybar}/bin/waybar
+  #         '';
+  #       RestartSec = 5;
+  #       Restart = "always";
+  #     };
+  #   };
     
-  };
+  # };
 }
--- users/default/sway/default.nix
+++ users/default/sway/default.nix
@@ -19,6 +19,7 @@ in {
 
   wayland.windowManager.sway = {
     config = {
+      # bars 
       floating.border = 1;
       gaps = {
         smartBorders = "on";
