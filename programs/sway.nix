76e8fd6d03ab51a61c7b23ef5ed882f18dcce688
“lugott” <lutz0go@gmail.com>
2022-02-08 14:57:47 +0100

    changed startship

--- programs/sway.nix
+++ programs/sway.nix
@@ -26,28 +26,28 @@
 #   })
 # ];
 
-  programs = {
+  # programs = {
 
-    sway = {
-      enable = true;
-      wrapperFeatures.gtk = true; # so that gtk works properly
-      extraPackages = with pkgs; [
-        brightnessctl
-        dmenu
-        foot
-        grim
-        kanshi
-        mako
-        slurp
-        swaylock
-        swayidle
-        waybar
-        wl-clipboard
-        wofi
-        wofi-emoji
-      ];
-    };
-  };
+  #   sway = {
+  #     enable = true;
+  #     wrapperFeatures.gtk = true; # so that gtk works properly
+  #     extraPackages = with pkgs; [
+  #       brightnessctl
+  #       dmenu
+  #       foot
+  #       grim
+  #       kanshi
+  #       mako
+  #       slurp
+  #       swaylock
+  #       swayidle
+  #       waybar
+  #       wl-clipboard
+  #       wofi
+  #       wofi-emoji
+  #     ];
+  #   };
+  # };
 
   services = {
     greetd = {
