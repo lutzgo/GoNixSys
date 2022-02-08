5b9206c070306ff86f10e7d7727937cf93ef4c69
“lugott” <lutz0go@gmail.com>
2022-02-08 15:36:01 +0100

    changed startship

---
+++ programs/gnome.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, lib, ... }:
+
+{
+
+  services.xserver = {
+    enabled = true;
+    displayManager = {
+      gdm.enable = true;
+      gdm.wayland = true;
+      # gdm.nvidiaWayland = true;
+      # autoLogin.enable = true;
+      # autoLogin.user = "lgo";
+    };
+    desktopManager.gnome = {
+      enable = true;
+    };
+    layout = "de";
+    # videoDrivers = [ "nvidia"];
+  };
+
+  flatpak.enable = true;
+
+  gnome.gnome-keyring.enable = true;
+
+  users.users.lgo.extraGroups = [ "flatpak" ];
+
+  xdg.portal.enable = true;
+
+}
