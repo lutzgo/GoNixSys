5d9777cabed636c6a452a9c274123dee8ffdeefe
“lugott” <lutz0go@gmail.com>
2022-02-08 15:03:05 +0100

    changed startship

--- programs/greetd.nix
+++ programs/greetd.nix
@@ -4,16 +4,18 @@
 
   programs = {
 
-  services = {
-    greetd = {
-      enable = true;
-      # package = pkgs.greetd.gtkgreet;
-      settings = {
-        default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
-          user = "lgo";
+    services = {
+      greetd = {
+        enable = true;
+        # package = pkgs.greetd.gtkgreet;
+        settings = {
+          default_session = {
+            command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+            user = "lgo";
+          };
         };
       };
+
     };
 
   };
