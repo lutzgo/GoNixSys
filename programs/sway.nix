491c8fdb24852f8e8a87a1f9b4db1d8d5a425791
“lugott” <lutz0go@gmail.com>
2022-02-11 15:45:51 +0100

    sway as sysremd.user.service

--- programs/sway.nix
+++ programs/sway.nix
@@ -2,29 +2,57 @@
 
 {
 
-#   nixpkgs.overlays = [
-#     (self: super: {
-#       wl-clipboard-x11 = super.stdenv.mkDerivation rec {
-#       pname = "wl-clipboard-x11";
-#       version = "5";
+  environment = {
+    etc = {
+      # Put config files in /etc. Note that you also can put these in ~/.config, but then you can't manage them with NixOS anymore!
+      "sway/config".source = ./dotfiles/sway/config;
+      "xdg/waybar/config".source = ./dotfiles/waybar/config;
+      "xdg/waybar/style.css".source = ./dotfiles/waybar/style.css;
+    };
 
-#       src = super.fetchFromGitHub {
-#         owner = "brunelli";
-#         repo = "wl-clipboard-x11";
-#         rev = "v${version}";
-#         sha256 = "1y7jv7rps0sdzmm859wn2l8q4pg2x35smcrm7mbfxn5vrga0bslb";
-#       };
+    systemPackages = with pkgs; [
+      (
+        pkgs.writeTextFile {
+          name = "startsway";
+          destination = "/bin/startsway";
+          executable = true;
+          text = ''
+            #! ${pkgs.bash}/bin/bash
+
+            # first import environment variables from the login manager
+            systemctl --user import-environment
+            # then start the service
+            exec systemctl --user start sway.service
+          '';
+        }
+      )
+    ];
+
+  };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      wl-clipboard-x11 = super.stdenv.mkDerivation rec {
+      pname = "wl-clipboard-x11";
+      version = "5";
+
+      src = super.fetchFromGitHub {
+        owner = "brunelli";
+        repo = "wl-clipboard-x11";
+        rev = "v${version}";
+        sha256 = "1y7jv7rps0sdzmm859wn2l8q4pg2x35smcrm7mbfxn5vrga0bslb";
+      };
   
-#       dontBuild = true;
-#       dontConfigure = true;
-#       propagatedBuildInputs = [ super.wl-clipboard ];
-#       makeFlags = [ "PREFIX=$(out)" ];
-#     };
+      dontBuild = true;
+      dontConfigure = true;
+      propagatedBuildInputs = [ super.wl-clipboard ];
+      makeFlags = [ "PREFIX=$(out)" ];
+    };
   
-#     xsel = self.wl-clipboard-x11;
-#     xclip = self.wl-clipboard-x11;
-#   })
-# ];
+    xsel = self.wl-clipboard-x11;
+    xclip = self.wl-clipboard-x11;
+  })
+];
 
   programs = {
 
@@ -46,20 +74,66 @@
         wofi-emoji
       ];
     };
+
+    waybar.enable = true;
+
   };
 
-  # services = {
-  #   greetd = {
-  #     enable = true;
-  #     # package = pkgs.greetd.gtkgreet;
-  #     settings = {
-  #       default_session = {
-  #         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
-  #         user = "lgo";
-  #       };
-  #     };
-  #   };
+  services.redshift = {
+    enable = true;
+    # Redshift with wayland support isn't present in nixos-19.09 atm. You have to cherry-pick the commit from https://github.com/NixOS/nixpkgs/pull/68285 to do that.
+    package = pkgs.redshift-wlr;
+  };
 
-  # };
+  systemd.user = {
+
+    targets.sway-session = {
+      description = "Sway compositor session";
+      documentation = [ "man:systemd.special(7)" ];
+      bindsTo = [ "graphical-session.target" ];
+      wants = [ "graphical-session-pre.target" ];
+      after = [ "graphical-session-pre.target" ];
+    };
+
+    services = {
+
+      sway = {
+        description = "Sway - Wayland window manager";
+        documentation = [ "man:sway(5)" ];
+        bindsTo = [ "graphical-session.target" ];
+        wants = [ "graphical-session-pre.target" ];
+        after = [ "graphical-session-pre.target" ];
+        # We explicitly unset PATH here, as we want it to be set by
+        # systemctl --user import-environment in startsway
+        environment.PATH = lib.mkForce null;
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = ''
+            ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
+          '';
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+      };
+
+      kanshi = {
+        description = "Kanshi output autoconfig ";
+        wantedBy = [ "graphical-session.target" ];
+        partOf = [ "graphical-session.target" ];
+        serviceConfig = {
+          # kanshi doesn't have an option to specifiy config file yet, so it looks
+          # at .config/kanshi/config
+          ExecStart = ''
+            ${pkgs.kanshi}/bin/kanshi
+          '';
+          RestartSec = 5;
+          Restart = "always";
+        };
+      };
+
+    };
+
+  };
 
 }
