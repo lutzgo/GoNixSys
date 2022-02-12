bf840f8375b5ab1de3b4e17fb71d5757c70f57a8
“lugott” <lutz0go@gmail.com>
2022-02-12 16:19:01 +0100

    added sway to home-manager

--- programs/sway.nix
+++ programs/sway.nix
@@ -93,15 +93,15 @@
       ];
     };
 
-    # fish = {
-    #   loginShellInit = ''
-    #     if test (id --user $USER) -ge 1000 && test (tty) = "/dev/tty1"
-    #     exec startsway
-    #     end
-    #   '';
-    # };
+    fish = {
+      loginShellInit = ''
+        if test (id --user $USER) -ge 1000 && test (tty) = "/dev/tty1"
+        exec startsway
+        end
+      '';
+    };
 
-    # waybar.enable = true;
+    waybar.enable = true;
 
   };
 
