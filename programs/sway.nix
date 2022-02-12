c2ca2668856d8868b09d58ec195298048f71b1a2
“lugott” <lutz0go@gmail.com>
2022-02-12 16:09:45 +0100

    removed sway as systemd

--- programs/sway.nix
+++ programs/sway.nix
@@ -2,49 +2,49 @@
 
 {
 
-  environment = {
-    # etc = {
-    #   # Put config files in /etc. Note that you also can put these in ~/.config, but then you can't manage them with NixOS anymore!
-    #   "sway/config".source = ./dotfiles/sway/config;
-    #   "xdg/waybar/config".source = ./dotfiles/waybar/config;
-    #   "xdg/waybar/style.css".source = ./dotfiles/waybar/style.css;
-    # };
+  # environment = {
+  #   # etc = {
+  #   #   # Put config files in /etc. Note that you also can put these in ~/.config, but then you can't manage them with NixOS anymore!
+  #   #   "sway/config".source = ./dotfiles/sway/config;
+  #   #   "xdg/waybar/config".source = ./dotfiles/waybar/config;
+  #   #   "xdg/waybar/style.css".source = ./dotfiles/waybar/style.css;
+  #   # };
 
-    systemPackages = with pkgs; [
-      (
-        pkgs.writeTextFile {
-          name = "startsway";
-          destination = "/bin/startsway";
-          executable = true;
-          text = ''
-            #! ${pkgs.bash}/bin/bash
+  #   systemPackages = with pkgs; [
+  #     (
+  #       pkgs.writeTextFile {
+  #         name = "startsway";
+  #         destination = "/bin/startsway";
+  #         executable = true;
+  #         text = ''
+  #           #! ${pkgs.bash}/bin/bash
 
-            # first import environment variables from the login manager
-            systemctl --user import-environment
-            # then start the service
-            exec systemctl --wait --user start sway.service
-          '';
-        }
-      )
+  #           # first import environment variables from the login manager
+  #           systemctl --user import-environment
+  #           # then start the service
+  #           exec systemctl --wait --user start sway.service
+  #         '';
+  #       }
+  #     )
 
-      # (
-      #   pkgs.writeTextFile {
-      #     name = "Sway Systemd Session";
-      #     destination = "/usr/share/wayland-sessions/sway-session.desktop";
-      #     executable = false;
-      #     text = ''
-      #       [Desktop Entry]
-      #       Name=Sway Systemd Session
-      #       Comment=SirCmpwn's Wayland window manager as a systemd service
-      #       Exec=/bin/startsway
-      #       Type=Application
-      #     '';
-      #   }
-      # )
+  #     # (
+  #     #   pkgs.writeTextFile {
+  #     #     name = "Sway Systemd Session";
+  #     #     destination = "/usr/share/wayland-sessions/sway-session.desktop";
+  #     #     executable = false;
+  #     #     text = ''
+  #     #       [Desktop Entry]
+  #     #       Name=Sway Systemd Session
+  #     #       Comment=SirCmpwn's Wayland window manager as a systemd service
+  #     #       Exec=/bin/startsway
+  #     #       Type=Application
+  #     #     '';
+  #     #   }
+  #     # )
 
-    ];
+  #   ];
 
-  };
+  # };
 
   nixpkgs.overlays = [
     (self: super: {
@@ -91,112 +91,110 @@
       ];
     };
 
-    fish = {
-      loginShellInit = ''
-        if test (id --user $USER) -ge 1000 && test (tty) = "/dev/tty1"
-        exec startsway
-        end
-      '';
-    };
+    # fish = {
+    #   loginShellInit = ''
+    #     if test (id --user $USER) -ge 1000 && test (tty) = "/dev/tty1"
+    #     exec startsway
+    #     end
+    #   '';
+    # };
 
     # waybar.enable = true;
 
   };
 
   # services = {
-  #   xserver = {
+  #   greetd = {
   #     enable = true;
-  #     displayManager = {
-  #       # defaultSession = "Sway Systemd Session";
-  #       gdm = {
-  #         enable = true;
-  #         wayland = true;
+  #     settings = {
+  #       default_session = {
+  #         command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
   #       };
   #     };
   #   };
   # };
 
-  systemd.user = {
+  # systemd.user = {
 
-    targets.sway-session = {
-      description = "Sway compositor session";
-      documentation = [ "man:systemd.special(7)" ];
-      bindsTo = [ "graphical-session.target" ];
-      wants = [ "graphical-session-pre.target" ];
-      after = [ "graphical-session-pre.target" ];
-    };
+  #   targets.sway-session = {
+  #     description = "Sway compositor session";
+  #     documentation = [ "man:systemd.special(7)" ];
+  #     bindsTo = [ "graphical-session.target" ];
+  #     wants = [ "graphical-session-pre.target" ];
+  #     after = [ "graphical-session-pre.target" ];
+  #   };
 
-    services = {
+  #   services = {
 
-      sway = {
-        description = "Sway - Wayland window manager";
-        documentation = [ "man:sway(5)" ];
-        bindsTo = [ "graphical-session.target" ];
-        wants = [ "graphical-session-pre.target" ];
-        after = [ "graphical-session-pre.target" ];
-        # We explicitly unset PATH here, as we want it to be set by
-        # systemctl --user import-environment in startsway
-        environment.PATH = lib.mkForce null;
-        serviceConfig = {
-          Type = "simple";
-          # ExecStartPre= ''
-          #   systemctl --user unset-environment WAYLAND_DISPLAY DISPLAY
-          # '';
-          ExecStart = ''
-            ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
-          '';
-          Restart = "on-failure";
-          RestartSec = 1;
-          TimeoutStopSec = 10;
-        };
-      };
+  #     sway = {
+  #       description = "Sway - Wayland window manager";
+  #       documentation = [ "man:sway(5)" ];
+  #       bindsTo = [ "graphical-session.target" ];
+  #       wants = [ "graphical-session-pre.target" ];
+  #       after = [ "graphical-session-pre.target" ];
+  #       # We explicitly unset PATH here, as we want it to be set by
+  #       # systemctl --user import-environment in startsway
+  #       environment.PATH = lib.mkForce null;
+  #       serviceConfig = {
+  #         Type = "simple";
+  #         # ExecStartPre= ''
+  #         #   systemctl --user unset-environment WAYLAND_DISPLAY DISPLAY
+  #         # '';
+  #         ExecStart = ''
+  #           ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
+  #         '';
+  #         Restart = "on-failure";
+  #         RestartSec = 1;
+  #         TimeoutStopSec = 10;
+  #       };
+  #     };
 
-      kanshi = {
-        enable = true;
-        description = "Kanshi output autoconfig";
-        wantedBy = [ "graphical-session.target" ];
-        partOf = [ "graphical-session.target" ];
-        serviceConfig = {
-          # kanshi doesn't have an option to specifiy config file yet, so it looks
-          # at .config/kanshi/config
-          ExecStart = ''
-            ${pkgs.kanshi}/bin/kanshi
-          '';
-          RestartSec = 5;
-          Restart = "always";
-        };
-      };
+  #     kanshi = {
+  #       enable = true;
+  #       description = "Kanshi output autoconfig";
+  #       wantedBy = [ "graphical-session.target" ];
+  #       partOf = [ "graphical-session.target" ];
+  #       serviceConfig = {
+  #         # kanshi doesn't have an option to specifiy config file yet, so it looks
+  #         # at .config/kanshi/config
+  #         ExecStart = ''
+  #           ${pkgs.kanshi}/bin/kanshi
+  #         '';
+  #         RestartSec = 5;
+  #         Restart = "always";
+  #       };
+  #     };
 
-      mako = {
-        enable = true;
-        description = "Mako notification daemon";
-        wantedBy = [ "graphical-session.target" ];
-        partOf = [ "graphical-session.target" ];
-        serviceConfig = {
-          ExecStart = ''
-            ${pkgs.mako}/bin/mako
-          '';
-          RestartSec = 5;
-          Restart = "always";
-        };
-      };
+  #     mako = {
+  #       enable = true;
+  #       description = "Mako notification daemon";
+  #       wantedBy = [ "graphical-session.target" ];
+  #       partOf = [ "graphical-session.target" ];
+  #       serviceConfig = {
+  #         ExecStart = ''
+  #           ${pkgs.mako}/bin/mako
+  #         '';
+  #         RestartSec = 5;
+  #         Restart = "always";
+  #       };
+  #     };
 
-      waybar = {
-        # enable = true;
-        description = "Wayland bar for Sway and Wlroots based compositors";
-        wantedBy = [ "graphical-session.target" ];
-        partOf = [ "graphical-session.target" ];
-        serviceConfig = {
-          ExecStart = ''
-            ${pkgs.waybar}/bin/waybar
-          '';
-          RestartSec = 5;
-          Restart = "always";
-        };
-      };
+  #     waybar = {
+  #       # enable = true;
+  #       description = "Wayland bar for Sway and Wlroots based compositors";
+  #       wantedBy = [ "graphical-session.target" ];
+  #       partOf = [ "graphical-session.target" ];
+  #       serviceConfig = {
+  #         ExecStart = ''
+  #           ${pkgs.waybar}/bin/waybar
+  #         '';
+  #         RestartSec = 5;
+  #         Restart = "always";
+  #       };
+  #     };
 
-    };
+  #   };
 
-  };
+  # };
 
 }
