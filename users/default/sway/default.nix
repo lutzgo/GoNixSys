c882b1355ecda41948259064d041e62b263168df
“lugott” <lutz0go@gmail.com>
2022-02-12 16:48:29 +0100

    configure sway

--- users/default/sway/default.nix
+++ users/default/sway/default.nix
@@ -2,6 +2,7 @@
 
 let
   background = "$HOME/.cache/backgrounds/greatwave_nord.jpg";
+  menu = "wofi";
 in
 
 {
@@ -37,6 +38,13 @@ in
   wayland.windowManager.sway = {
     enable = true;
     config = {
+
+      fonts = {
+        names = [ "Roboto" ];
+        style = "Regular Bold";
+        size = 12.0;
+      };
+
       gaps = {
         outer = 5;
         smartBorders = "on";
@@ -46,6 +54,7 @@ in
       window.border = 1;
       bars = [ ];
 
+
       input = {
         "*" = {
           xkb_layout = "de";
@@ -53,6 +62,20 @@ in
           xkb_options = "grp:alt_shift_toggle";
           xkb_numlock = "enable";
         };
+      };
+
+      menu = menu;
+
+      keybindings = {
+        # Exit sway (logs you out of your Wayland session)
+        "${modifier}+Shift+e" = "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
+        # Reload the configuration file
+        "${modifier}+Shift+r" = "reload";
+        # Kill focused window
+        "${modifier}+Shift+q" = "kill";
+        "${modifier}+d" = "exec ${menu}";
+        # Launch the default terminal. $TERM is defined in ../alacritty.nix line 11
+        "${modifier}+x" = "exec foot-client";
       };
 
     };
