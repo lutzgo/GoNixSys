2cb1e61b5aa9d5f586f7fa4504c4bbc6be69ad8e
“lugott” <lutz0go@gmail.com>
2022-02-13 09:33:36 +0100

    color for foot

--- users/default/foot.nix
+++ users/default/foot.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  programs.foot = { 
+  programs.foot = let colorscheme = config.colorscheme; in { 
     enable = true;
     server.enable = true;
     settings = {
@@ -13,13 +13,18 @@
         title = "foot";
 
         font = "Fira Code:size=11";
-        box-drawings-uses-font-glyphs = "yes";
+        box-drawings-uses-font-glyphs = "no";
         bold-text-in-bright = "yes";
         dpi-aware = "yes";
       };
 
       mouse = {
         hide-when-typing = "yes";
+      };
+
+      colors = {
+        background = "#${colorscheme.colors.base00}";
+        foreground = "#${colorscheme.colors.base05}";
       };
     };
   };
