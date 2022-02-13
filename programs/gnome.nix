01dc0584275e5f62f74a20ce6b46ab5861d36cb1
“lugott” <lutz0go@gmail.com>
2022-02-13 10:00:49 +0100

    added gnome to flake

--- programs/gnome.nix
+++ programs/gnome.nix
@@ -2,6 +2,24 @@
 
 {
 
+  environment.gnome.excludePackages = with pkgs; [ 
+    gnome.cheese 
+    gnome-photos 
+    gnome.gnome-music 
+    gnome.gnome-terminal 
+    gnome.gedit 
+    epiphany 
+    evince 
+    gnome.gnome-characters 
+    gnome.totem 
+    gnome.tali 
+    gnome.iagno 
+    gnome.hitori 
+    gnome.atomix 
+    gnome-tour  
+    gnome.geary
+  ];
+
   services = {
       xserver = {
         enable = true;
